e1604f7b33192bdceefdb57eb75557ea4afb46af
oldman <597800451@qq.com>
2023-02-23 13:26:06 +0800

    WFIL

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -122,7 +122,7 @@
                         </svg>
                         </span>
 
-                          <span style="opacity: .8" class="ml-2 cursor-pointer" @click="copyAddress">
+                          <span style="opacity: .8" class="ml-2 cursor-pointer copy-text" @click="copyAddress">
 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-clipboard-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1Zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5v-1Zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2Z"/>
 </svg>                        </span>
@@ -451,10 +451,21 @@ export default {
       window.location.href = `https://explorer.glif.io/address/${this.contractAddress}/?network=hyperspace`
     },
     copyAddress(){
-      new Clipboard('.tag',{
+
+      let clipboard = new Clipboard('.copy-text', {
         text:  () => {
           return this.contractAddress
         }
+      })
+      clipboard.on('success', e => {
+        console.log(e)
+        // 释放内存
+        clipboard.destroy()
+      })
+      clipboard.on('error', e => {
+        console.error(e)
+
+        clipboard.destroy()
       })
     }
   },
